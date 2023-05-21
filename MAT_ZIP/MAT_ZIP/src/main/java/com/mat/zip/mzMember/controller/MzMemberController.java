999f2c5408c6307c24096e107ed81ffb33a6806f
DESKTOP-QM9I7MN\Administrator <tjgus027@kakao.com>
2023-05-22 04:44:53 +0900

    카카오 로그인 api 구현 파일 추가

--- MAT_ZIP/MAT_ZIP/src/main/java/com/mat/zip/mzMember/controller/MzMemberController.java
+++ MAT_ZIP/MAT_ZIP/src/main/java/com/mat/zip/mzMember/controller/MzMemberController.java
@@ -6,12 +6,17 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.mat.zip.mzMember.model.KakaoLoginBO;
 import com.mat.zip.mzMember.model.MzMemberDTO;
 import com.mat.zip.mzMember.service.MzMemberserviceImpl;
 
@@ -20,9 +25,16 @@ import com.mat.zip.mzMember.service.MzMemberserviceImpl;
 public class MzMemberController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MzMemberController.class);
+	private KakaoLoginBO kakaoLoginBO;
 	
 	@Inject
 	MzMemberserviceImpl service;
+	
+	/* 카카오 의존성 주입 */
+	@Autowired
+	private void setKakaoLoginBO(KakaoLoginBO kakaologinBO) {
+		this.kakaoLoginBO = kakaologinBO;
+	}
 	
 	/** 이메일 인증 */
 	@RequestMapping(value = "mailCheck", method = RequestMethod.GET)
@@ -33,9 +45,21 @@ public class MzMemberController {
 		return service.authEmail(email);
 	}
 	
-	/** 메인 페이지에서 로그인 페이지 넘어가는 경로 맵핑 */
-	@RequestMapping("login")
-	public void loginPage() {
+	/** 메인 페이지에서 로그인 페이지 넘어가는 경로 맵핑, 네이버 아이디 인증 url 생성 메서드 */
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String loginPage(HttpSession session, Model model, String url) {
+		
+		/* naverLogin 클래스의 네아로 인증 url 생성하는 메서드 호출 */
+		//String naverAuthUrl = naverLogin.getAuthorizationURL(session);
+//		model.addAttribute("naverUrl", naverAuthUrl);
+//		logger.info("naver url: " + naverAuthUrl);
+		
+		/* kakaoLoginBO 클래스의 카카오 로그인 인증 url 생성하는 메서드 호출 */
+		String kakaoAuthUrl = kakaoLoginBO.getAuthorizationURL(session);
+		model.addAttribute("kakaoUrl", kakaoAuthUrl);
+		logger.info("kakao url: " + kakaoAuthUrl);
+		
+		return "/mz_member/login";
 	}
 	
 	/** 로그인 
@@ -43,10 +67,9 @@ public class MzMemberController {
 	 * 유효하지 않으면 (0이라면) 세션 값에 null 넣고 메세지에 miss 넣어서 값 전달
 	 * 유효하다면 회원 정보를 가져와서 세션에 넣고 메인페이지로 리다이렉션 */
 	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String loginTest(MzMemberDTO dto, HttpServletRequest request, RedirectAttributes rttr) throws Exception {
+	public String loginTest(MzMemberDTO dto, HttpServletRequest request, RedirectAttributes rttr, HttpSession session) throws Exception {
 		logger.info("로그인 실행");
 		
-		HttpSession session = request.getSession();
 		int loginResult = service.memberLogin(dto);
 		
 		if (loginResult == 0) {
@@ -65,6 +88,26 @@ public class MzMemberController {
 			return "redirect:/index.jsp";
 		}
 	}
+	
+	/* 카카오 로그인 성공 시 콜백 메서드 */
+	@RequestMapping(value = "callbackKakao", method = {RequestMethod.GET, RequestMethod.POST})
+	public String loginKakao(Model model, @RequestParam String code, @RequestParam String state, HttpSession session) throws Exception {
+		logger.info("카카오 로그인 콜백 성공");
+		//JsonParser json = new JsonParser();
+		
+		OAuth2AccessToken oauthToken;
+		oauthToken = kakaoLoginBO.getAccessToken(session, code, state); 
+		String apiResult = kakaoLoginBO.getUserProfile(oauthToken);
+		System.out.println(apiResult);
+		
+		//json.changeJson(apiResult);
+		
+		model.addAttribute("memberInfo", apiResult);
+		System.out.println(model);
+		
+		return "/mz_member/callbackKakao";
+	}
+	
 	
 	/** 로그아웃 
 	 * 로그아웃 실행 시에 세션 무효화하고 메인페이지로 리다이렉션 */
---
+++ MAT_ZIP/MAT_ZIP/src/main/webapp/WEB-INF/views/mz_member/callbackKakao.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>카카오 로그인</title>
+<script type="text/javascript" src="https://static.nid.naver.com/js/naverLogin_implicit-1.0.2.js" charset="utf-8"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+<script type="text/javascript">
+  $(document).ready(function() {
+	  
+    });
+</script>
+<div style="background-color: #15a181; width: 100%; height: 50px; text-align: center; color: white;">
+    <h3>Kakao_Login Success</h3>
+  </div>
+  <br>
+  <h2 style="text-align: center" id="name"></h2>
+  <h4 style="text-align: center" id="email"></h4>
+  
+  <script>
+    $(function () {
+      $("body").hide();
+      $("body").fadeIn(1000);  // 1초 뒤에 사라짐 
+     
+      setTimeout(function(){$("body").fadeOut(1000);},1000);
+      setTimeout(function(){location.href= "${pageContext.request.contextPath}/index.jsp"},2000);
+		// 2초 뒤에 메인 화면으로  
+    
+    })
+  </script>
+</body>
+</html>

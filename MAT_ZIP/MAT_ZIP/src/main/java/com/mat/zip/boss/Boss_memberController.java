ef267cf3ac290938dc9966c3b4ead2e589b83fbb
JayMon <kioplm0211@gmail.com>
2023-05-27 17:26:04 +0900

    충돌 파일처리

--- MAT_ZIP/MAT_ZIP/src/main/java/com/mat/zip/boss/Boss_memberController.java
+++ MAT_ZIP/MAT_ZIP/src/main/java/com/mat/zip/boss/Boss_memberController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mat.zip.mzMember.model.MzMemberDAOImpl;
+import com.mat.zip.mzMember.model.MzMemberDTO;
+import com.mat.zip.mzMember.service.MzMemberserviceImpl;
+
 	@Controller
 	public class Boss_memberController {
 	    @Autowired
-	    Boss_memberDAO boss_memberDAO;
+	    Boss_memberDAO dao;
 	    @Autowired
-	    MemberDAO memberDAO;
+	    MzMemberDAOImpl dao2;
 	    
 	    @GetMapping("/boss_login")
 	    public String loginPage(HttpServletRequest request, HttpSession session) {
@@ -30,24 +34,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 	    }
 
 	    @PostMapping("/boss/member_login")
-	    public String member_login(MemberVO bag, HttpSession session, HttpServletRequest request, Model model) {
+	    public String member_login(MzMemberDTO bag, HttpSession session, HttpServletRequest request, Model model) {
 	        System.out.println(bag + "login실행됨");
 	        
-	        // Member 테이블 로그인
-	        MemberVO vo = memberDAO.login(bag);
+	        // Member 테이블 회원체크
+	        int check = dao2.memberLogin(bag);
+	        //부여된 세션을 스트링으로 변경하여 변수 저장
+	        String userId = String.valueOf(session.getAttribute("user_id"));
+	        //변수를 dto에 넣어서 값 멤버 vo 값 저장
+	        MzMemberDTO dto = dao2.getMemberInfo(userId);
 	        
 	        // Boss_member 테이블 로그인
 	        Boss_memberVO bossBag = new Boss_memberVO();
 	        bossBag.setUser_id(bag.getUser_id());
 	        bossBag.setPassword(bag.getPassword());
-	        Boss_memberVO bossVo = boss_memberDAO.login(bossBag);
-
-	        // member 또는 boss 테이블 중 하나라도 로그인 성공 시
-	        if (vo != null || bossVo != null) {
-	            if (vo != null) {
+	        Boss_memberVO bossVo = dao.login(bossBag);
+	        
+	        //멤버 vo의 아이디와 boss vo의 아이디가 같으면 if문 실행
+	        if(dto.getUser_id().equals(bossVo.getUser_id())) {
+	        	
+	            if (check != 0) {
 	                // Member 로그인 성공 시, user_id와 nickName을 세션에 저장
-	                session.setAttribute("user_id", vo.getUser_id());
-	                session.setAttribute("nickName", vo.getNickName());
+	                session.setAttribute("user_id", dto.getUser_id());
+	                session.setAttribute("nickName", dto.getNickName());
 	            }
 
 	            if (bossVo != null) {
@@ -79,6 +88,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 	} 
 	    
+	    //사장회원등록 메서드 
+//	    @PostMapping("/boss/innerJoinAndInsert")
+//	    public String innerJoinAndInsert(@RequestParam String store_id, HttpSession session) {
+//	    	// 세션에서 현재 로그인한 사용자의 ID를 가져옵니다.
+//	    	String user_id = (String) session.getAttribute("user_id");
+//
+//	    	// bag에 사용자 ID와 store_id를 저장합니다.
+//	    	Boss_memberVO bag = new Boss_memberVO();
+//	    	bag.setUser_id(user_id);
+//	    	bag.setStore_id(store_id);
+//
+//	    	// member 테이블과 bossmember 테이블을 inner join해서 회원가입 처리를 합니다.
+//	    	dao.innerJoinAndInsert(bag);
+//	    	// 이제 이 사용자는 사장님 계정으로도 로그인되어 있으므로, 'boss_id' 세션에도 사용자 ID를 저장합니다.
+//	        session.setAttribute("boss_id", user_id);
+//	        // log 확인용 (삭제가능)
+//	    	System.out.println("사장회원가입완료");
+//	    	 System.out.println("store_id: " + store_id);
+//	    	// 회원가입 처리가 완료되면 메인 페이지로 리다이렉트합니다.
+//	    	return "forward:board_index.jsp";
+//	    }
 	    
 
 

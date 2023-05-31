31cc09f1c3da83135e9879b2311a956dc407840a
DESKTOP-QM9I7MN\Administrator <tjgus027@kakao.com>
2023-05-31 17:21:07 +0900

    챗봇 관련 파일 추가 (백엔드: m,c,service / 프론트: js, css)

---
+++ MAT_ZIP/MAT_ZIP/src/main/webapp/resources/js/chatbot.js
@@ -0,0 +1,70 @@
+	let websocket;
+	
+	//소켓 연결
+	function connect() {
+		//소켓 객체 생성
+		var wsUri = 'ws://localhost:8898/zip/chatbot';
+		websocket = new WebSocket(wsUri);
+		//console.log("연결됨" + websocket);
+		websocket.onopen = onOpen;
+		websocket.onmessage = onMessage;
+	}
+	
+	//웹소켓에 연결되었을때 호출 함수
+	function onOpen(){
+		console.log("연결 ok: " + websocket);
+	}
+	
+	function sendMessage(element) {
+		websocket.send(element.value);
+	}
+	
+	// 메시지 수신
+	function onMessage(res) {
+		let temp;
+		console.log("데이터: " + res);
+		
+		JSON.parse(res.data).bubbles.forEach(bubble => {
+			if(bubble.type == "text") { //text로 넘어오면 전달값 text 생성
+				temp = $("<span>", {class: "msgBox"});
+				temp.append(
+						$("<span>", {text: bubble.data.description})
+						);
+				temp.append('<br>');
+			} else { //template, 버튼으로 생성
+				temp = $("<span>", {class: "msgBox"});
+				temp.append(
+						$("<span>", {text: bubble.data.cover.data.description})
+						);
+				$('#chatList').append(temp);
+				temp.append('<br>');
+				
+				temp = $("<span>", {class: "msgBox"});
+				bubble.data.contentTable.forEach(content => {
+					temp.append(
+							$("<button>", {
+								text: content[0].data.title,
+								value: content[0].data.data.action.data.postbackFull,
+								class: "suggestBtn",
+								onclick: "sendMessage(this)"
+								})
+							);
+				temp.append('<br>');
+				}) //foreach
+			} //else
+				
+			$('#chatList').append(temp);
+			
+			//자동 스크롤 내리는 함수
+			$('#chatList').animate({scrollTop: $('#chatList')[0].scrollHeight}, 600);
+		})
+	} //onMessage
+	
+	function chatToggle() {
+		$('#chatMain').toggle();
+		$('#chatOpenBtn').toggle();
+	}
+	
+	
+	//jsp 실행과 동시에 연결
+	connect();
